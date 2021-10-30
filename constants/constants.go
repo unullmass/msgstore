5d80e81d02deb3bbd9351cb0c9f09479d147d73d
unullmass <codemanure@live.co.uk>
2021-10-30 21:02:43 +0530

    Constants

---
+++ constants/constants.go
@@ -0,0 +1,17 @@
+package constants
+
+// routing
+const (
+	RootPrefix  = "/mydata"
+	DocIdPath   = "docid"
+	StartTsPath = "startTimestamp"
+	EndTsPath   = "endTimestamp"
+	TsPath      = "timestamp"
+	KeyPath     = "key"
+	ValuePath   = "value"
+)
+
+//
+const (
+	MaxRecordsReturn = 500
+)
